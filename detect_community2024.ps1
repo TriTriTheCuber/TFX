770e398c685b1bb710c2de34c7248f3856cae0ba
TriTriTheCuber <121834111+TriTriTheCuber@users.noreply.github.com>
2025-05-23 22:55:02 -0400

    Add files via upload

---
+++ detect_community2024.ps1
@@ -0,0 +1,15 @@
+# detect_community.ps1
+
+$cfgPath = "$env:AppData\Microsoft Flight Simulator 2024\UserCfg.opt"
+
+if (Test-Path $cfgPath) {
+    $line = Select-String 'InstalledPackagesPath' -Path $cfgPath
+    if ($line) {
+        $match = $line.Line -replace '.*\"(.*)\".*', '$1\Community'
+        $match | Set-Content -Path '_community_path.txt' -Encoding ASCII
+    } else {
+        'NO_INSTALLED_PATH' | Set-Content -Path '_community_path.txt' -Encoding ASCII
+    }
+} else {
+    'NOT_FOUND' | Set-Content -Path '_community_path.txt' -Encoding ASCII
+}
